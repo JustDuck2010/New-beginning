215cb79b3ff73971b3ee60278329f41eee700b36
Igor <yakv-2@yandex.ru>
2026-06-02 12:20:26 +0300

    S

--- Dictionary drills Order filler.py
+++ Dictionary drills Order filler.py
@@ -1,12 +1,13 @@
 def fillable(stock, merch, n):
-    #print(stock.items(), merch)
     if merch not in stock:
         return False
     if stock.get(merch, False) > n:
         return(True)
+    else:
+        return False
     
 print(fillable({
             'football': 4,
             'boardgame': 10,
             'leggos': 1,
-            'doll': 5 }, "football", 3))
+            'doll': 5 }, "leggos", 3))
